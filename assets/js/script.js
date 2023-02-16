0686311ee5b37adc040ea1e365441439ff17554f
mackenzie-wentworth <mwentworth28@gmail.com>
2023-02-16 12:32:07 -0500

    added comments to js to plan out code

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Timer
+// Create array for questions
+// Write a function to show a question based on an index
+// Track high scores using local storage
+// Clear all high scores using local storage
