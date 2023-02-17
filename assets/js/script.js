d7709058095430b097143911b017653fbb2957d3
mackenzie-wentworth <mwentworth28@gmail.com>
2023-02-17 13:12:29 -0500

    begin structuring js and css to populate html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,18 @@
 // Track high scores using local storage
 // Clear all high scores using local storage
 
+// Variables
+
+var score = 0;
+var questionIndex = 0;
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTimer");
+var questionsSection = document.querySelector("#questionsSection");
+var quizContainer = document.querySelector("#quizContainer");
+var allScores = JSON.parse(localStorage.getItem("allScores")) || [];
+var questionTitle = document.createElement("h2");
+var ulEl = document.createElement("ul");
+var listItem = document.createElement("li");
 
 // Timer function
 function timerCountdown() {
