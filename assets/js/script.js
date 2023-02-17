36de707b19edbcaf3d3eb44894c4564142f91a0b
mackenzie-wentworth <mwentworth28@gmail.com>
2023-02-17 13:25:44 -0500

    Added js quiz questions. Added console logs to view quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,14 +17,89 @@ var questionTitle = document.createElement("h2");
 var ulEl = document.createElement("ul");
 var listItem = document.createElement("li");
 
-// Timer function
-function timerCountdown() {
-    var timeLeft = 75;
-}
+// Quiz time remaining
 
-var timeInterval = setInterval (function() {
-    if (timeLeft >= 0) {
-        clearInterval (timeInterval);
-        displayFinalScore();
-    } 1000;
-});
+var secondsLeft = 75;
+
+// Interval time
+
+var holdInterval = 0;
+
+// Penalty 10 seconds
+
+var penalty = 10;
+
+var questions = [
+    {
+        title: "This is Question One",
+        options: ["choice A", "choice B", "choice C", "choice D"],
+        answer: "choice A"
+    },
+    {
+        title: "This is Question Two",
+        options: ["choice A", "choice B", "choice C", "choice D"],
+        answer: "choice B"
+    },
+]
+
+//check high scores
+console.log(allScores);
+// Check questions array in console log
+
+console.log(questions);
+
+// Create ul for quiz questions
+
+console.log(ulEl);
+console.log(timer);
+if (timer !== null) {
+    timer.addEventListener("click", function () {
+        if (holdInterval === 0) {
+            holdInterval = setInterval(function () {
+                secondsLeft--;
+                currentTime.textContent = secondsLeft + " seconds";
+
+                if (secondsLeft <= 0) {
+                    clearInterval(holdInterval);
+                    quizComplete();
+                    currentTime.textContent = "OOOPS! OUT OF TIME!";
+                }
+            }, 1000);
+        }
+        render(questionIndex);
+    });
+}
+console.log(questionIndex);
+
+// Renders questions
+
+function render(questionIndex) {
+
+    // Clears existing data 
+
+    questionsSection.innerHTML = "";
+    questionTitle.innerHTML = "";
+    ulEl.innerHTML = "";
+
+
+
+    // Loop through questions array
+
+    for (var i = 0; i < questions.length; i++) {
+        // Appends question title only
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].options;
+        questionsSection.appendChild(questionTitle);
+        questionTitle.textContent = userQuestion;
+    }
+    // New for each for question
+
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.setAttribute("id", "questionli");
+        listItem.textContent = newItem;
+        questionsSection.appendChild(ulEl);
+        ulEl.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
